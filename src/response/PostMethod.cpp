31ccbe10df2553fec1efbdcf9353596221132527
nandajfa <nanda.jfa@gmail.com>
2023-09-30 17:35:26 -0300

    feat: add function setResponse

--- src/response/PostMethod.cpp
+++ src/response/PostMethod.cpp
@@ -6,7 +6,7 @@
 /*   By: jefernan <jefernan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 10:27:05 by jefernan          #+#    #+#             */
-/*   Updated: 2023/09/30 16:54:31 by jefernan         ###   ########.fr       */
+/*   Updated: 2023/09/30 17:21:58 by jefernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,51 +21,47 @@ PostMethod::~PostMethod()
 PostMethod::PostMethod(HttpRequest request): _httpRequest(request)
 {}
 
-void    PostMethod::handleForm(){
-    std::string body = _httpRequest.getBody();
-    std::istringstream ss(body);
-    std::string pair;
+std::string PostMethod::handleMethod(std::string uri)
+{
+    uri = " ";
 
-    while (std::getline(ss, pair, '&'))
-    {
-        size_t equal = pair.find('=');
-        if (equal != std::string::npos)
-        {
-            std::string fieldName = pair.substr(0, equal);
-            std::string fielddata = pair.substr(equal + 1);
-            for (size_t i = 0; i < fielddata.length(); ++i)
-            {
-                if (fielddata[i] == '+')
-                    fielddata[i] = ' ';
-            }
-            _formData[fieldName] = fielddata + "\r\n";
-        }
+    if (_httpRequest._has_body){
+        if (_httpRequest._has_multipart)
+            handleMultipart();
+        if (_httpRequest._has_form)
+            handleForm();
+        setResponse("200", "OK", "<html><body><h1>200 OK</h1></body></html>");
+        Logger::info << "Post request completed successfully." << std::endl;
     }
-    print();
+    else if (!_httpRequest._has_body)
+    {
+        setResponse("204", "No content","<html><body><h1>204 No Content</h1></body></html>");
+        Logger::info << "No content." << std::endl;
+    }
+    else
+    {
+        setResponse("500", "Internal Server Error", "<html><body><h1>500  Internal Server Error</h1></body></html>");
+        Logger::info << "Error in post request." << std::endl;
+    }
+
+    return (_responseHeader);
 }
 
-void PostMethod::saveFile(size_t pos, const std::string& value, const std::string& data)
+void    PostMethod::handleMultipart()
 {
-    std::size_t filenameEnd = data.find("\"", pos);
+    std::string boundary = _httpRequest.getBoundary();
+    std::string body = _httpRequest.getBody();
 
-    if (filenameEnd != std::string::npos)
+    _formData.clear();
+    size_t pos = 0;
+    while ((pos = body.find(boundary, pos)) != std::string::npos)
     {
-        std::string fileName = data.substr(pos, filenameEnd - pos);
-        std::string filePath = "/uploads/" + fileName;
-        for ( int i = 0; i < 10; i++ ) {
-            filePath += static_cast<char>( rand() % 26 + 97 );
-        }
-
-        std::ofstream file(filePath.c_str(), std::ios::binary);
-        if (file.is_open()){
-
-            file.write(value.c_str(), value.length());
-            file.close();
-            created = true;
-        } else {
-            created = false;
-        }
+        pos += boundary.length();
+        size_t endPos = body.find(boundary, pos);
+        if (endPos != std::string::npos)
+            parseMultipartFormData(pos, endPos);
     }
+    print();
 }
 
 void    PostMethod::parseMultipartFormData(size_t pos, size_t endPos)
@@ -106,19 +102,49 @@ void    PostMethod::parseMultipartFormData(size_t pos, size_t endPos)
     }
 }
 
-void    PostMethod::handleMultipart()
+void PostMethod::saveFile(size_t pos, const std::string& value, const std::string& data)
 {
-    std::string boundary = _httpRequest.getBoundary();
-    std::string body = _httpRequest.getBody();
+    std::size_t filenameEnd = data.find("\"", pos);
 
-    _formData.clear();
-    size_t pos = 0;
-    while ((pos = body.find(boundary, pos)) != std::string::npos)
+    if (filenameEnd != std::string::npos)
     {
-        pos += boundary.length();
-        size_t endPos = body.find(boundary, pos);
-        if (endPos != std::string::npos)
-            parseMultipartFormData(pos, endPos);
+        std::string fileName = data.substr(pos, filenameEnd - pos);
+        std::string filePath = "/uploads/" + fileName;
+        for ( int i = 0; i < 10; i++ ) {
+            filePath += static_cast<char>( rand() % 26 + 97 );
+        }
+
+        std::ofstream file(filePath.c_str(), std::ios::binary);
+        if (file.is_open()){
+
+            file.write(value.c_str(), value.length());
+            file.close();
+            created = true;
+        } else {
+            created = false;
+        }
+    }
+}
+
+void    PostMethod::handleForm(){
+    std::string body = _httpRequest.getBody();
+    std::istringstream ss(body);
+    std::string pair;
+
+    while (std::getline(ss, pair, '&'))
+    {
+        size_t equal = pair.find('=');
+        if (equal != std::string::npos)
+        {
+            std::string fieldName = pair.substr(0, equal);
+            std::string fielddata = pair.substr(equal + 1);
+            for (size_t i = 0; i < fielddata.length(); ++i)
+            {
+                if (fielddata[i] == '+')
+                    fielddata[i] = ' ';
+            }
+            _formData[fieldName] = fielddata + "\r\n";
+        }
     }
     print();
 }
@@ -137,47 +163,12 @@ void    PostMethod::print()
     std::cout << RESET << std::endl;
 }
 
-std::string PostMethod::handleMethod(std::string uri)
-{
-    uri = " ";
-    std::string responseHeader;
-
-    if (_httpRequest._has_body){
-        if (_httpRequest._has_multipart)
-            handleMultipart();
-        if (_httpRequest._has_form)
-            handleForm();
-        _response.version = "HTTP/1.1";
-        _response.status_code = "200";
-        _response.status_message = "OK";
-        _response.content_type = "text/html";
-        _response.content_length = "0";
-        _response.body = "<html><body><h1>200 OK</h1></body></html>";
-        responseHeader = assembleResponse();
-        Logger::info << "Post request completed successfully." << std::endl;
-    }
-    else if (!_httpRequest._has_body)
-    {
-        _response.version = "HTTP/1.1";
-        _response.status_code = "204";
-        _response.status_message = "No content";
-        _response.content_type = "text/html";
-        _response.content_length = "0";
-        _response.body = "<html><body><h1>204 No Content</h1></body></html>";
-        responseHeader = assembleResponse();
-        Logger::info << "No content." << std::endl;
-    }
-    else
-    {
-        _response.version = "HTTP/1.1";
-        _response.status_code = "500";
-        _response.status_message = "Internal Server Error";
-        _response.content_type = "text/html";
-        _response.content_length = "0";
-        _response.body = "<html><body><h1>500  Internal Server Error</h1></body></html>";
-        responseHeader = assembleResponse();
-        Logger::info << "Error in post request." << std::endl;
-    }
-
-    return (responseHeader);
+void    PostMethod::setResponse(std::string status, std::string message, std::string body){
+    _response.version = "HTTP/1.1";
+    _response.status_code = status;
+    _response.status_message = message;
+    _response.content_type = "text/html";
+    _response.content_length = "0";
+    _response.body = body;
+    _responseHeader = assembleResponse();
 }
