7210bb2efb531960456e645e8f484ccddc54cb90
Mitsu325 <patricia.mashiba@gmail.com>
2023-10-12 15:38:27 -0300

    chore: change  define value

--- includes/Libs.hpp
+++ includes/Libs.hpp
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 22:12:50 by pmitsuko          #+#    #+#             */
-/*   Updated: 2023/10/11 16:04:17 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2023/10/12 15:32:35 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@
 
 #define IP "127.0.0.1"
 
-#define DEFAULT_ROOT "/var/www/html"
+#define DEFAULT_ROOT "www/html"
 #define DEFAULT_ERROR_ROOT "www/error"
 
 #define RESET_COLOR "\033[0m"
@@ -46,7 +46,7 @@
 #define HTTP_VERSION_NOT_SUPPORTED "505"
 #define ENTITY_TOO_LARGE "413"
 
-#define MAX_BODY_SIZE 100
+// #define MAX_BODY_SIZE 100
 
 struct responseData {
     std::string content;
